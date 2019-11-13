50668d0f0db32df28de01abbd11265dbe4e33b59
patryk406 <Merph406@gmail.com>
2019-11-13 16:19:52 +0100

    Add some tips, change icon upload, test with bg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Weather from './app_component/weather.component';
-import Form from './app_component/form.component';
-import './App.css';
+import React from "react";
+import Weather from "./app_component/weather.component";
+import Form from "./app_component/form.component";
+import "./App.css";
 
-import 'weather-icons/css/weather-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //api.openweathermap.org/data/2.5/weather?q=London
-const API_key = 'e614a40be0d49993168759d8b8e25e37';
+const API_key = "e614a40be0d49993168759d8b8e25e37";
 
 class App extends React.Component {
   constructor() {
@@ -22,22 +22,23 @@ class App extends React.Component {
       celsius: undefined,
       temp_max: undefined,
       temp_min: undefined,
-      description: '',
-      pressure: '',
-      wind: '',
-      sunrise: '',
-      sunset: '',
+      description: "",
+      pressure: "",
+      wind: "",
+      sunrise: "",
+      sunset: "",
+      tips: "",
       error: false
     };
     //setting icons for our app.
     this.weatherIcon = {
-      Thunderstorm: 'wi-thunderstorm',
-      Drizzle: 'wi-sleet',
-      Rain: 'wi-storm-shower',
-      Snow: 'wi-snow',
-      Atmosphere: 'wi-fog',
-      Clear: 'wi-day-sunny',
-      Clouds: 'wi-day-fog'
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-shower",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
     };
   }
   calcRise(sunrise) {
@@ -59,7 +60,7 @@ class App extends React.Component {
   //Specific parrameters to set corect icon for our app.
 
   //nesting needed data to link for our app.
-  get_WeatherIcon(icons, rangeId) {
+  get_WeatherIcon(rangeId) {
     switch (true) {
       case rangeId >= 200 && rangeId <= 232:
         this.setState({ icon: this.weatherIcon.Thunderstorm });
@@ -86,7 +87,60 @@ class App extends React.Component {
         this.setState({ icon: this.weatherIcon.Clouds });
     }
   }
-
+  getTip(Id) {
+    switch (true) {
+      case Id >= 200 && Id <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        this.setState({
+          tips:
+            "Beatifull sigh of nature, powerfull and devatating power. Just something incredible but better stay at home today, don't risk your life for stupid stroll"
+        });
+        break;
+      case Id >= 300 && Id <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        this.setState({
+          tips:
+            "Refreshing drizzle but be careful , visibility in roads geting worse!"
+        });
+        break;
+      case Id >= 500 && Id <= 531:
+        this.setState({
+          tips:
+            "There is no better time to watch TV in warm blanked near fireplace or listen to music of falling rain drops."
+        });
+        break;
+      case Id >= 600 && Id <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        this.setState({
+          tips:
+            "Perfect day to have fun in snor like in old times, take a ride in sleigh to forest, tage a trip to mountains or have romantic dinner with your love surrounded by light of candle"
+        });
+        break;
+      case Id >= 701 && Id <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere });
+        this.setState({
+          tips:
+            "Be careful, visibility is bad, better don't drive to fast and watch out for yourself. That what we don't see is the scariest thing."
+        });
+        break;
+      case Id === 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        this.setState({
+          tips:
+            "Clear beatifull day, perfect moment to take a stroll or sightseeing something near. Don't waste than time"
+        });
+        break;
+      case Id >= 801 && Id <= 804:
+        this.setState({ icon: this.weatherIcon.Clouds });
+        this.setState({
+          tips:
+            "Clouds are beatifull sight of nature with many interesting but also scary shapes. Find fun of guess what you see!"
+        });
+        break;
+      default:
+        this.setState({ tips: "its rainy" });
+    }
+  }
   getWeather = async e => {
     e.preventDefault();
 
@@ -111,15 +165,16 @@ class App extends React.Component {
         description: response.weather[0].main
       });
       this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+      this.getTip(response.weather[0].id);
     } else {
-      this.setState.error = 'true';
+      this.setState.error = "true";
     }
   };
 
   //Rendering properties from weather and reforming them using the data from serv then setting to our app.
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Form loadweather={this.getWeather} error={this.error} />
         <Weather
           city={this.state.city}
@@ -133,6 +188,7 @@ class App extends React.Component {
           sunset={this.state.sunset}
           description={this.state.description}
           weatherIcon={this.state.icon}
+          tips={this.state.tips}
         />
       </div>
     );
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./weather.style.css";
 const Weather = props => {
   return (
-    <div className='container'>
-      <div className='cards pt-4'>
+    <div className="container">
+      <div className="cards pt-4">
         <h1>{props.city}</h1>
-        <h5 className='py-4'>
+        <h5 className="icon_tip py-4 d-flex">
           <i className={`wi ${props.weatherIcon} display-1`}></i>
+          <p className="tip col-6">{props.tips}</p>
         </h5>
 
         {props.temp_celsius ? (
-          <h1 className='py-2'>Actual Temp {props.temp_celsius}&deg;</h1>
+          <h1 className="py-2">Actual Temp {props.temp_celsius}&deg;</h1>
         ) : null}
         {/* {show max and min temp} */}
         {minmaxTemp(props.temp_max, props.temp_min)}
         {pressWind(props.pressure, props.wind)}
         {riseSet(props.sunrise, props.sunset)}
-        <h4 className='py-3'>{props.description}</h4>
+        <h4 className="py-3">{props.description}</h4>
       </div>
     </div>
   );
@@ -24,32 +26,32 @@ const Weather = props => {
 function riseSet(sunrise, sunset) {
   if (sunrise && sunset) {
     return (
-      <div className='card-deck '>
-        <div className='card bg-transparent border-0 p-2'>
-          <div className='card-title'>
+      <div className="card-deck ">
+        <div className="card bg-transparent border-0 p-2">
+          <div className="card-title">
             <h3>
               Sunrise
               <img
-                src='https://img.icons8.com/office/30/000000/sunrise.png'
-                alt='rise'
+                src="https://img.icons8.com/office/30/000000/sunrise.png"
+                alt="rise"
               ></img>
             </h3>
           </div>
-          <div className='card-text'>
+          <div className="card-text">
             <h4>{sunrise}</h4>
           </div>
         </div>
-        <div className='card bg-transparent border-0 p-2'>
-          <div className='card-title'>
+        <div className="card bg-transparent border-0 p-2">
+          <div className="card-title">
             <h3>
               Sunset
               <img
-                src='https://img.icons8.com/cotton/40/000000/sunset--v4.png'
-                alt='set'
+                src="https://img.icons8.com/cotton/40/000000/sunset--v4.png"
+                alt="set"
               ></img>
             </h3>
           </div>
-          <div className='card-text'>
+          <div className="card-text">
             <h4>{sunset}</h4>
           </div>
         </div>
@@ -61,8 +63,8 @@ function pressWind(pressure, wind) {
   if (pressure && wind) {
     return (
       <h3>
-        <span className='px-4'>Pressure: {pressure} hPa</span>
-        <span className='px-4'>Wind Speed: {wind} m/s</span>
+        <span className="px-4">Pressure: {pressure} hPa</span>
+        <span className="px-4">Wind Speed: {wind} m/s</span>
       </h3>
     );
   }
@@ -71,8 +73,8 @@ function minmaxTemp(max, min) {
   if (min && max) {
     return (
       <h3>
-        <span className='px-4'>Min Temperature {min}&deg;</span>
-        <span className='px-4'>Max Temperature {max}&deg;</span>
+        <span className="px-4">Min Temperature {min}&deg;</span>
+        <span className="px-4">Max Temperature {max}&deg;</span>
       </h3>
     );
   }
